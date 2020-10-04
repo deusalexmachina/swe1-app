2b6c089ce19d2a64037d0aec3c38800c624d2114
Alex Hong <ah4927@nyu.edu>
2020-10-04 18:14:09 -0400

    settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,6 +136,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
